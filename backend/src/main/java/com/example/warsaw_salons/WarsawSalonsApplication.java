f84da3409b19c3e780332d9d7c1a738f22307258
stivig5 <5stivig5@gmail.com>
2026-06-05 01:06:53 +0200

    Initial commit: Setup Spring Boot backend and React Vite frontend

---
+++ backend/src/main/java/com/example/warsaw_salons/WarsawSalonsApplication.java
@@ -0,0 +1,13 @@
+package com.example.warsaw_salons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarsawSalonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarsawSalonsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/warsaw_salons/WarsawSalonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.warsaw_salons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarsawSalonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
